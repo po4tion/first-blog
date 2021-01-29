365c0417f65cb0d0f61f2816e756003b66c6945b
DongKyu <po4tion0429@gmail.com>
2021-01-30 00:51:59 +0900

    post filter

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -67,15 +67,23 @@ export const list = async (ctx) => {
     ctx.status = 400;
     return;
   }
+
+  const { tag, username } = ctx.query;
+
+  const query = {
+    ...(username ? { 'user.username': username } : {}),
+    ...(tag ? { tags: tag } : {}),
+  };
+
   try {
-    const posts = await Post.find()
+    const posts = await Post.find(query)
       .sort({ _id: -1 })
       .limit(10)
       .skip((page - 1) * 10)
       .lean()
       .exec();
 
-    const postCount = await Post.countDocuments().exec();
+    const postCount = await Post.countDocuments(query).exec();
     console.log(postCount);
     ctx.set('Last-Page', Math.ceil(postCount / 10));
     ctx.body = posts.map((post) => ({
